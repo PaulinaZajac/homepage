1da8f686152cd19b918f7e6a1be2c1237bebd208
Paulina Zajac <paulinazajac.mail@gmail.com>
2020-08-26 16:25:46 +0200

    js change

---
+++ main.js
@@ -0,0 +1,14 @@
+const firstName = "Paulina";
+const age = 27;
+
+console.log(
+    `Hello I'm ${firstName} and I have ${age} years old`
+    );
+
+    const heading = document.querySelector('.third-heading--js');
+    console.log(heading);
+
+    console.log(heading.innerHTML);
+    heading.innerHTML = 'blabla';
+    
+    console.log(heading.innerHTML);
